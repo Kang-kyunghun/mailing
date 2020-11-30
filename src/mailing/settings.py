bc16b3a02875a71b1eb2f2b67e605dfc464a7ff3
Jack-earlysloth <kgh239@earlysloth.com>
2020-12-01 01:06:53 +0900

    불필요한 셋팅 제거

--- src/mailing/settings.py
+++ src/mailing/settings.py
@@ -74,23 +74,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mailing.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-# DATABASES = {
-#     'default' : {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'herren',
-#         'USER': 'root',
-#         'PASSWORD': '!kucie271150',
-#         'HOST': 'kyunghun-database.colrmog3tb1d.ap-northeast-2.rds.amazonaws.com',
-#         'PORT': '3306',
-#     }
-# }
-
-
-
 DATABASES = {
     'default' : {
         'ENGINE': 'django.db.backends.mysql',
